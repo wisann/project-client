f1ab36a1471395238cce759a87eb56c0d444db37
wisan <121894013+wisann@users.noreply.github.com>
2023-06-11 14:20:41 -0300

    backend project using spring boot

---
+++ backend_client/src/main/java/io/github/wisan/backend/model/Client.java
@@ -0,0 +1,60 @@
+package io.github.wisan.backend.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "client")
+public class Client {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	private String name;
+
+	private String email;
+	
+	public Client() {
+		
+	}	
+	
+
+	public Client(long id, String name, String email) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+	}
+
+
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+}
---
+++ backend_client/src/main/java/io/github/wisan/backend/repository/ClientRepository.java
@@ -0,0 +1,12 @@
+package io.github.wisan.backend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import io.github.wisan.backend.model.Client;
+
+
+@Repository
+public interface ClientRepository extends JpaRepository<Client, Long>{
+
+}
